3a100f34f82b9acbf453fb70acc5a4341cc3f892
jeffgat <jeff.gtbn@gmail.com>
2023-05-17 16:23:20 -0600

    init with vite

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import Board from './components/Board.vue';
+import './assets/globals.scss';
+</script>
+
+<template>
+  <Board msg="CHESS BOARD" />
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
